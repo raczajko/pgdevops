e620ef9f637da56733652fbb73294c101a64260d
Denis Lussier <denisl@openscg.com>
2016-08-29 21:03:40 +0000

    it's bigsql.devops baby!

--- bin/remove_service.bat
+++ bin/remove_service.bat
@@ -5,7 +5,7 @@
 @REM ###################################################
 
 set CWD=%~sdp0
-set BAM_SERVICE_NAME="bigsql.bam2"
+set BAM_SERVICE_NAME="bigsql.devops"
 set BAM_INSTALL="%CWD%bam.exe"
 
 %BAM_INSTALL% //DS//%BAM_SERVICE_NAME%
