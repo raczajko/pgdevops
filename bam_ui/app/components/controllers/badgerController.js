47ca18e80f0ba368c5757e390f8d24c0f324ba5c
Kiran Moka <kiran.moka@openscg.com>
2016-11-22 17:01:23 +0530

    pgBadger javascript changes.

--- bam_ui/app/components/controllers/badgerController.js
+++ bam_ui/app/components/controllers/badgerController.js
@@ -115,9 +115,10 @@ angular.module('bigSQL.components').controller('badgerController', ['$scope', '$
         $scope.report_file = "";
         $scope.report_url = "";
         var selectedFiles = [];
+        $scope.badgerError = '';
         
         var selectLog = document.getElementsByName("selectLog");
-        for (var i=0;i<selectLog.length-1; i++){
+        for (var i=0;i<selectLog.length; i++){
             if(selectLog[i].checked){
                 selectedFiles.push(selectLog[i].value);
             }
