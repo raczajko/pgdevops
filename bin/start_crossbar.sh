cecdcb2cfe225b0269f922fe45a02db32e0e724f
Mahesh Balumuri <mahesh.balumuri@openscg.com>
2016-11-29 15:16:27 +0530

    updated startcrossbar.sh

--- bin/start_crossbar.sh
+++ bin/start_crossbar.sh
@@ -5,5 +5,5 @@
 #############################################
 
 DIR=$(cd "$(dirname "$0")"; pwd)
-#export PYTHONPATH=$DIR/../lib
-python $DIR/start_crossbar.py start --cbdir $DIR #--loglevel info --logtofile --logdir $DIR/../../data/logs/bam2
+export PYTHONPATH=$DIR/../lib
+python $DIR/start_crossbar.py start --cbdir $DIR --loglevel info --logtofile --logdir $DIR/../../data/logs/devops
