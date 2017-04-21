421daf2c77965800b43fab401418e366ac98b087
Kiran Moka <kiran.moka@openscg.com>
2017-04-21 11:57:48 +0530

    Error message tweak.

--- bam_ui/app/components/controllers/generateBadgerReportController.js
+++ bam_ui/app/components/controllers/generateBadgerReportController.js
@@ -25,7 +25,7 @@ angular.module('bigSQL.components').controller('generateBadgerReportController',
         });
 
         if($uibModalInstance.smallFiles.length > 0){
-            $scope.badgerError = $uibModalInstance.smallFiles + '. There is insufficient lines file(s) to produce report.'
+            $scope.badgerError = $uibModalInstance.smallFiles + '. The chosen logfile(s) do not contain enough data to generate a pgBadger report.'
         }else if ($uibModalInstance.selectedFiles.length > 0) {
             $scope.generatingReportSpinner = true;
             var args={
