0aae8ed4f7bfa97af3224afdf6182c761d154004
Kiran Kumar Moka <kiran.moka@openscg.com>
2017-05-02 14:53:07 +0530

    Passing time stamp as parameter for every api call to resolve IE cache issue.

--- bam_ui/app/components/services/bamAjaxCall.js
+++ bam_ui/app/components/services/bamAjaxCall.js
@@ -1,13 +1,23 @@
 angular.module('bigSQL.components').factory('bamAjaxCall', function ($q, $http, $window) {
 
 
-    var getCmdData = function (cmd, params={}) {
+    var getCmdData = function (cmd, params) {
+
+        var isObject = function(a) {
+            return (!!a) && (a.constructor === Object);
+        };
+        if (isObject(params)){
+        var data_params = params;    
+        } else{
+            var data_params={};
+        }
 
         return $q(function (resolve, reject) {
-            var config = {}
-            if(Object.keys(params).length>0){
+            var config = {};
+            data_params['_pgd'] = Math.floor(Date.now() / 1000);
+            if(Object.keys(data_params).length>0){
                 config = {
-                params: params,
+                params: data_params,
                 headers : {'Accept' : 'application/json'}
 
                 };
