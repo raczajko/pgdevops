587011e5bc9acdd019e8682bcc07c31faa18c210
Kiran Kumar Moka <kiran.moka@openscg.com>
2017-05-03 11:38:47 +0530

    Fixed issue #BIGSQL-826

--- bam_ui/app/components/controllers/feedbackFormController.js
+++ bam_ui/app/components/controllers/feedbackFormController.js
@@ -1,12 +1,16 @@
 angular.module('bigSQL.components').controller('feedbackFormController', ['$scope', '$uibModalInstance', 'PubSubService', '$rootScope', '$uibModal', 'bamAjaxCall', function ($scope, $uibModalInstance, PubSubService, $rootScope, $uibModal, bamAjaxCall) {
 
 	$scope.lab = $uibModalInstance.lab;
-	$scope.disp_name = $uibModalInstance.disp_name;  
+	$scope.subject = $uibModalInstance.disp_name;  
 
 	var userInfoData = bamAjaxCall.getCmdData('userinfo');
         userInfoData.then(function(data) {
         	$scope.email = data.email;
         }); 
+
+    $scope.sendEmail = function (argument) {
+    	$uibModalInstance.dismiss('cancel');
+    }
 
     $scope.cancel = function () {
         $uibModalInstance.dismiss('cancel');
