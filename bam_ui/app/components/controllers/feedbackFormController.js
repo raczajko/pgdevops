706da001096fa155a44cb4d9e8c4245da089612a
Kiran Kumar Moka <kiran.moka@openscg.com>
2017-05-08 18:17:11 +0530

    Changes for #BIGSQL-810.

--- bam_ui/app/components/controllers/feedbackFormController.js
+++ bam_ui/app/components/controllers/feedbackFormController.js
@@ -1,16 +1,27 @@
-angular.module('bigSQL.components').controller('feedbackFormController', ['$scope', '$uibModalInstance', 'PubSubService', '$rootScope', '$uibModal', 'bamAjaxCall', function ($scope, $uibModalInstance, PubSubService, $rootScope, $uibModal, bamAjaxCall) {
+angular.module('bigSQL.components').controller('feedbackFormController', ['$scope', '$uibModalInstance', 'PubSubService', '$rootScope', '$uibModal', 'bamAjaxCall', '$http', function ($scope, $uibModalInstance, PubSubService, $rootScope, $uibModal, bamAjaxCall, $http) {
 
 	$scope.lab = $uibModalInstance.lab;
+	$scope.to_email = 'bigsql-feedback@openscg.com';
 	if($uibModalInstance.disp_name){
 		$scope.subject = $uibModalInstance.disp_name + ' Lab';
 	}
 
 	var userInfoData = bamAjaxCall.getCmdData('userinfo');
         userInfoData.then(function(data) {
-        	$scope.email = data.email;
+        	$scope.from_email = data.email;
         }); 
 
     $scope.sendEmail = function (argument) {
+    	var args = {
+    		'text' : $scope.subject,
+    		'subject' : $scope.feedback,
+    		'from_email' : $scope.from_email,
+    		'to' : $scope.to_email
+    	}
+    	
+    	var sendFeedback = $http.post('https://bigsql.org/email-feedback',args)
+    	sendFeedback.then(function (argument) {	
+    	})
     	$uibModalInstance.dismiss('cancel');
     }
 
