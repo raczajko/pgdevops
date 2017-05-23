d5c5fe3b6c0fa060ebb7d8f64b59ccc9ff9678a9
Kiran Kumar Moka <kiran.moka@openscg.com>
2017-05-23 16:58:25 +0530

    Static message tweaks.

--- bam_ui/app/common/services/htmlMessages.js
+++ bam_ui/app/common/services/htmlMessages.js
@@ -2,7 +2,7 @@ angular.module('bigSQL.common').factory('htmlMessages', function ( $q, $filter) 
 
     var allMessages = {
         'labNotEnabled': 'This is a labs feature, you need to enable this in <a href="#/components/settings">settings.</a>',
-        'pwd-or-ssh-msg' : 'Please provide either Password or SSH Key'
+        'pwd-or-ssh-msg' : 'Please provide either password or SSH Key',
     };
 
     return {
