c141512a7cee24425a4365101298958f951d61d7
Kiran Kumar Moka <kiran.moka@openscg.com>
2017-05-23 15:16:27 +0530

    Fix for #BIGSQL-938

--- bam_ui/app/common/services/htmlMessages.js
+++ bam_ui/app/common/services/htmlMessages.js
@@ -1,7 +1,8 @@
 angular.module('bigSQL.common').factory('htmlMessages', function ( $q, $filter) {
 
     var allMessages = {
-        "labNotEnabled": 'This is a labs feature, you need to enable this in <a href="#/components/settings">settings.</a>'
+        'labNotEnabled': 'This is a labs feature, you need to enable this in <a href="#/components/settings">settings.</a>',
+        'pwd-or-ssh-msg' : 'Please provide either Password or SSH Key'
     };
 
     return {
