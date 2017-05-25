489029225a4760d208d3d42d7f038d1446361308
Kiran Kumar Moka <kiran.moka@openscg.com>
2017-05-25 15:28:46 +0530

    Code changes as per #BIGSQL-944

--- bam_ui/app/components/controllers/rdsModalController.js
+++ bam_ui/app/components/controllers/rdsModalController.js
@@ -1,8 +1,12 @@
-angular.module('bigSQL.components').controller('rdsModalController', ['$scope', '$uibModalInstance', 'PubSubService', 'UpdateComponentsService', 'MachineInfo', '$http', '$window', '$interval', '$rootScope', 'bamAjaxCall', function ($scope, $uibModalInstance, PubSubService, UpdateComponentsService, MachineInfo, $http, $window, $interval, $rootScope, bamAjaxCall) {
+angular.module('bigSQL.components').controller('rdsModalController', ['$scope', '$uibModalInstance', 'PubSubService', 'UpdateComponentsService', 'MachineInfo', '$http', '$window', '$interval', '$rootScope', 'bamAjaxCall', 'htmlMessages', function ($scope, $uibModalInstance, PubSubService, UpdateComponentsService, MachineInfo, $http, $window, $interval, $rootScope, bamAjaxCall, htmlMessages) {
 
     $scope.loadingSpinner = true;
     $scope.lab = $uibModalInstance.lab;
     $scope.disp_name = $uibModalInstance.disp_name;
+    $scope.availList = [];
+    var addList = [];
+    $scope.addToMetadata = false;
+    $scope.discoverMsg = htmlMessages.getMessage('discover-rds');
 
     var rdslist = bamAjaxCall.getCmdData('rdslist');
     rdslist.then(function (data) {
@@ -12,8 +16,42 @@ angular.module('bigSQL.components').controller('rdsModalController', ['$scope', 
             $rootScope.$emit('disableLab', $scope.lab, 'off')
         }else{
             $scope.rdsList = data;
+            for (var i = $scope.rdsList.length - 1; i >= 0; i--) {
+                if($scope.rdsList[i].status == 'available'){
+                    $scope.availList.push($scope.rdsList[i]);
+                }
+            }
         }
     });
+
+    $scope.createConnPgadmin = function(index){
+        $scope.addToMetadata = true;
+        $scope.addToMetadataMsg = htmlMessages.getMessage('add-to-pgadmin');
+        for (var i = $scope.availList.length - 1; i >= 0; i--) {
+            if($scope.availList[i].selected){
+                var args = {};
+                args['db'] = $scope.availList[i].dbname;
+                args['port'] = $scope.availList[i].port;
+                args['user'] = $scope.availList[i].master_user;
+                args['host'] = $scope.availList[i].address;
+                args['component'] = $scope.availList[i].instance;
+                args['project'] = 'aws-rds';
+                args['rds'] = true;
+                var addToMetaData = $http.post($window.location.origin + '/api/add_to_metadata', args);
+            }
+        }
+        $uibModalInstance.dismiss('cancel');
+    };
+
+    $scope.optionToggled = function(){
+        $scope.checked = false;
+        angular.forEach($scope.availList, function (item) {
+            if(item.selected){
+                $scope.checked = true;
+            }
+        });
+    }
+
 
     $scope.closeAlert = function (index) {
         $scope.alerts.splice(index, 1);
