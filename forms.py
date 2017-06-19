a8b582aeabe6eefc0483ddd5e7aa23a28706241d
Kiran Kumar Moka <kiran.moka@openscg.com>
2017-06-19 17:31:42 +0530

    Added AMI validation.

--- forms.py
+++ forms.py
@@ -1,14 +1,23 @@
 from wtforms import StringField, ValidationError, HiddenField
 from flask_security.forms import RegisterForm, EqualTo, password_required
 from wtforms.validators import InputRequired, Email, Length
+from Components import Components as pgc
 
 class RegisterForm(RegisterForm):
-    ami_form = True
+    data = pgc.get_data("lablist")
+    ami_form = False
+    for lab in data:
+    	if(lab['lab'] == 'aws-rds' and lab['enabled'] == 'on'):
+    		ami_form = True
+
     if ami_form:
         ami_id = StringField('AWS instance Id', validators=[Length(max=50)])
 
         def validate_ami_id(form, field):
-            #raise ValidationError('AWS instance Id if needs to be valied')
-            pass
+            validationData = pgc.get_data("verify-ami", str(field.data))
+            if validationData[0]['state'] == 'error':
+               raise ValidationError(validationData[0]['msg'])
+	    else:
+	       pass
     else:
         pass
