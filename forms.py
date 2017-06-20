2e7d9e6a39d69212809f593662405347f5ad2374
Kiran Kumar Moka <kiran.moka@openscg.com>
2017-06-20 18:28:22 +0530

    Changes for #BIGSQL-983

--- forms.py
+++ forms.py
@@ -13,33 +13,31 @@ pgc_scripts_path = os.path.join(PGC_HOME, 'hub', 'scripts')
 if pgc_scripts_path not in sys.path:
     sys.path.append(pgc_scripts_path)
 
+def check_ami(ami_id="pgdevops"):
+	cmd=os.path.join(PGC_HOME,"pgc")
+	pgcCmd = "{0} {1} {2} {3}".format(cmd,"verify-ami",ami_id , "--json")
+	rc=0
+	msg=""
+	try:
+		process = subprocess.check_output(pgcCmd,
+					  shell=True)
+		final_out = json.loads(process.strip().decode('ascii'))[0]
+		rc=process.returncode
+		msg=str(final_out['msg'])
+	except Exception as e:
+		final_out = json.loads(e.output.strip().decode('ascii'))[0]
+		rc = e.returncode
+		msg = str(final_out['msg'])
+	return {"rc": rc, "msg": msg}
+
 class RegisterForm(RegisterForm):
-
-	def check_ami(ami_id="pgdevops"):
-		cmd=os.path.join(PGC_HOME,"pgc")
-		pgcCmd = "{0} {1} {2} {3}".format(cmd,"verify-ami",ami_id , "--json")
-		rc=0
-		msg=""
-		try:
-			process = subprocess.check_output(pgcCmd,
-                          shell=True)
-			final_out = json.loads(process.strip().decode('ascii'))[0]
-			rc=process.returncode
-			msg=str(final_out['msg'])
-		except Exception as e:
-			final_out = json.loads(e.output.strip().decode('ascii'))[0]
-			rc = e.returncode
-			msg = str(final_out['msg'])
-		return {"rc": rc, "msg": msg}
-
    	checkAMI = check_ami()
-	if checkAMI.get("rc")!=2:
+	if checkAMI.get('rc')!=2:
 		ami_form = True
-		ami_id = StringField('AWS instance Id', validators=[Length(max=50)])
-
+		ami_id = StringField('AMI Instance Id', validators=[Length(max=50)])
 		def validate_ami_id(form, field):
-			validationData = pgc.get_data("verify-ami", str(field.data))
-			if validationData[0]['state'] == 'error':
-				raise ValidationError(validationData[0]['msg'])
+			validationData = check_ami(str(field.data))
+			if validationData['rc'] != 0:
+				raise ValidationError(validationData['msg'])
 			else:
 				pass
