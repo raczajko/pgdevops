66865fb6ede238435b62e6e7a3abe2edc60ed905
Kiran Kumar Moka <kiran.moka@openscg.com>
2017-07-18 10:34:26 +0530

    HTML changes for create rds instance.

--- bam_ui/app/common/directives/leadingZero.js
+++
@@ -1,9 +0,0 @@
-angular.module('bigSQL.common', []).filter('digits', function() {
-return function(input) {
-   if (input < 10) {
-          input = '0' + input;
-      }
-
-      return input;
-    }
-});
