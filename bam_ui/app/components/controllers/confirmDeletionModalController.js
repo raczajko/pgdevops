48bcc2843346851c1320d34e0633b82d1ef70998
Kiran Kumar Moka <kiran.moka@openscg.com>
2017-08-18 15:18:14 +0530

    Credentials delete modal UI tweaks.

--- bam_ui/app/components/controllers/confirmDeletionModalController.js
+++ bam_ui/app/components/controllers/confirmDeletionModalController.js
@@ -1,6 +1,7 @@
 angular.module('bigSQL.components').controller('confirmDeletionModalController', ['$scope','$rootScope', '$uibModalInstance','MachineInfo', 'PubSubService', '$window', '$http', 'bamAjaxCall', function ($scope, $rootScope, $uibModalInstance, MachineInfo, PubSubService, $window, $http, bamAjaxCall) {
 
 	var session;
+    $scope.deleting = false;
 
     var sessionPromise = PubSubService.getSession();
     sessionPromise.then(function (val) {
@@ -32,8 +33,10 @@ angular.module('bigSQL.components').controller('confirmDeletionModalController',
     }
 
     $scope.removeCreds = function (argument) {
+        $scope.deleting = true;
         var deleteCred = bamAjaxCall.deleteData('/api/pgc/credentials/' + deleteFiles.toString() )
         deleteCred.then(function (data) {
+            $scope.deleting = false;
             $rootScope.$emit('deleteResponse', data);
             $rootScope.$emit('refreshCreds');
             $uibModalInstance.dismiss('cancel');
