a6faee35e8f4a5707f857cfd251ab628fefd605f
Kiran Kumar Moka <kiran.moka@openscg.com>
2017-09-06 12:47:57 +0530

    Javascript Improvements for add host modal.

--- bam_ui/app/components/controllers/addHostController.js
+++ bam_ui/app/components/controllers/addHostController.js
@@ -244,9 +244,11 @@ angular.module('bigSQL.components').controller('addHostController', ['$scope', '
 		var getCredentials = bamAjaxCall.getCmdData('pgc/credentials/')
 		getCredentials.then(function (data) {
 			$scope.gettingCreds = false;
-			$scope.credentialsList = $(data.data).filter(function(i,n){ return n.cred_type != "cloud" ;})
-			if ($scope.cred_name) {
-				$scope.selected_cred_name = $scope.cred_name;
+			if (data.data.length>0) {
+				$scope.credentialsList = $(data.data).filter(function(i,n){ return n.cred_type != "cloud" ;})
+				if ($scope.cred_name) {
+					$scope.selected_cred_name = $scope.cred_name;
+				}
 			}
 		})
 	}
