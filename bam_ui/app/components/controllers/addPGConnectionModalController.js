fc92e03f231097c07ea238500cc15449475eac2b
naveenkoppula <naveen.koppula@openscg.com>
2017-10-24 10:45:05 +0530

    Made server groups dynamic #BIGSQL-1316

--- bam_ui/app/components/controllers/addPGConnectionModalController.js
+++ bam_ui/app/components/controllers/addPGConnectionModalController.js
@@ -6,8 +6,15 @@ angular.module('bigSQL.components').controller('addPGConnectionModalController',
 	$scope.pgList = $uibModalInstance.pgList;
 	$scope.editConnData = $uibModalInstance.editConnData;
 	$scope.servers = [];
+	$scope.servergroups = [];
 	$scope.port = 5432;
 
+    var server_group_list = bamAjaxCall.getData("/admin/browser/nodes/");
+    server_group_list.then(function (argument) {
+        $scope.servergroups = argument.data;
+        if($scope.servergroups.length > 0 && !$scope.selectedServer )
+          $scope.selectedServer = $scope.servergroups[0].label;
+    });
 	for (var i = 0; i < $scope.pgList.length; ++i) {
 	    var obj = $scope.pgList[i];
 	    if ($scope.servers.indexOf(obj.server_group)<0) {
@@ -17,7 +24,6 @@ angular.module('bigSQL.components').controller('addPGConnectionModalController',
 	if ($scope.servers.length < 1) {
 		$scope.servers = ['Servers'];
 	}
-	$scope.selectedServer = $scope.servers[0];
 	$scope.tryToConnect = false;
 	$scope.connectionStatus = false;
 	$scope.installingStatus = false;
@@ -32,7 +38,7 @@ angular.module('bigSQL.components').controller('addPGConnectionModalController',
 
     $scope.create_btn = "Create";
 	if($scope.editConnData){
-		$scope.type = 'Edit';
+	    $scope.type = 'Edit';
 		$scope.create_btn = "Update";
 		$scope.host = $scope.editConnData.host;
 		$scope.port = parseInt($scope.editConnData.port);
@@ -58,41 +64,85 @@ angular.module('bigSQL.components').controller('addPGConnectionModalController',
             checkUser.then(function (data) {
                 if(data.error == 0){
                     if ($scope.pgList.length > 0) {
-                        $scope.gid = parseInt($($scope.pgList).filter(function(i,n){ return n.server_group ==  $scope.selectedServer})[0].gid);
+                        $scope.gid = parseInt($($scope.servergroups).filter(function(i,n){ return n.label ==  $scope.selectedServer})[0]._id);
+                        var data = {
+                            component:$scope.connectionName,
+                            host: $scope.host,
+                            port: $scope.port,
+                            database: $scope.database,
+                            user: $scope.userName,
+                            gid: $scope.gid,
+                            rds : false
+                        };
+                        if ($scope.sid) {
+                            data.sid=$scope.sid;
+                        }
+                        var addToMetaData = bamAjaxCall.postData('/api/add_to_metadata', data);
+                        addToMetaData.then(function (argument) {
+                            if(!$scope.sid){
+                                $scope.sid = argument.sid;
+                            }
+                            if ($scope.savePassword) {
+                                $rootScope.$emit('getDBstatus', $scope.sid, $scope.gid, $scope.password, true, true);
+                                $scope.connect_err = '';
+                                $scope.connecting = true;
+                            }else{
+                                $rootScope.$emit('getDBstatus', $scope.sid, $scope.gid, $scope.password, false, true);
+                                $scope.connect_err = '';
+                                $scope.connecting = true;
+                            }
+                            $rootScope.$emit('refreshPgList');
+                            $uibModalInstance.dismiss('cancel');
+                            $rootScope.$emit('closePasswordModal');
+                        });
                     }else{
                         $scope.gid = "1";
+                        var server_group_list = bamAjaxCall.getData("/admin/browser/nodes/");
+                        server_group_list.then(function (argument) {
+                            if(argument.data.length > 0){
+                                var defaultGroup = argument.data.filter(function(i,n){ return i.label == "Servers"})[0];
+                                if(defaultGroup){
+                                    $scope.gid = parseInt(defaultGroup._id)
+                                }else{
+                                    $scope.gid = argument.data[0]._id;
+                                }
+                            }else{
+
+                            }
+                            var data = {
+                                component:$scope.connectionName,
+                                host: $scope.host,
+                                port: $scope.port,
+                                database: $scope.database,
+                                user: $scope.userName,
+                                gid: $scope.gid,
+                                rds : false
+                            };
+                            if ($scope.sid) {
+                                data.sid=$scope.sid;
+                            }
+                            var addToMetaData = bamAjaxCall.postData('/api/add_to_metadata', data);
+                            addToMetaData.then(function (argument) {
+                                if(!$scope.sid){
+                                    $scope.sid = argument.sid;
+                                }
+                                if ($scope.savePassword) {
+                                    $rootScope.$emit('getDBstatus', $scope.sid, $scope.gid, $scope.password, true, true);
+                                    $scope.connect_err = '';
+                                    $scope.connecting = true;
+                                }else{
+                                    $rootScope.$emit('getDBstatus', $scope.sid, $scope.gid, $scope.password, false, true);
+                                    $scope.connect_err = '';
+                                    $scope.connecting = true;
+                                }
+                                $rootScope.$emit('refreshPgList');
+                                $uibModalInstance.dismiss('cancel');
+                                $rootScope.$emit('closePasswordModal');
+                            });
+                        });
                     }
-                    var data = {
-                        component:$scope.connectionName,
-                        host: $scope.host,
-                        port: $scope.port,
-                        database: $scope.database,
-                        user: $scope.userName,
-                        gid: $scope.gid,
-                        rds : false
-                    };
-                    if ($scope.sid) {
-                        data.sid=$scope.sid;
-                    }
-                    var addToMetaData = bamAjaxCall.postData('/api/add_to_metadata', data);
-                    addToMetaData.then(function (argument) {
-                        if(!$scope.sid){
-                            $scope.sid = argument.sid;
-                        }
-                        if ($scope.savePassword) {
-                            $rootScope.$emit('getDBstatus', $scope.sid, $scope.gid, $scope.password, true, true);
-                            $scope.connect_err = '';
-                            $scope.connecting = true;
-                        }else{
-                            $rootScope.$emit('getDBstatus', $scope.sid, $scope.gid, $scope.password, false, true);
-                            $scope.connect_err = '';
-                            $scope.connecting = true;
-                        }
-                        $rootScope.$emit('refreshPgList');
-                        $uibModalInstance.dismiss('cancel');
-                        $rootScope.$emit('closePasswordModal');
-                    });
-                }else{
+                }
+                else{
                     $scope.connectionError = true;
                     $scope.message = data.msg;
                     $scope.connecting = false;
